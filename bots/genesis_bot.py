eb27d7d1721031cc21bfc85ad98d606a7d0dad18
Hoffalo <lorenzohoff2006@gmail.com>
2025-04-02 12:45:16 +0000

    genesis bot creation

---
+++ bots/genesis_bot.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from collections import deque
+import random
+
+class GenesisBot:
+    def __init__(self, action_size):
+        pass
+    
+    def act(self, state):
+        pass
